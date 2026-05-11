58da8fb5f17a3108e1d1814da9afc2bf86cb26a8
number6823 <ohdehet@naver.com>
2026-05-11 12:40:43 +0900

    MainLayout 작성/ 테마 팔레트 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { RouterProvider } from "react-router";
 import GetRouter from "./router/GetRouter.tsx";
+import { ThemeProvider } from "styled-components";
+import { lightTheme } from "./styles/theme.ts";
 
-function App () {
-  return <RouterProvider router= {GetRouter}>
-
-
-  </RouterProvider>;
+function App() {
+    return (
+        <ThemeProvider theme={lightTheme}>
+            <RouterProvider router={GetRouter}></RouterProvider>
+        </ThemeProvider>
+    );
 }
-export default App
+export default App;
--- src/router/GetRouter.tsx
+++ src/router/GetRouter.tsx
@@ -2,12 +2,17 @@ import { createBrowserRouter } from "react-router";
 import HomePage from "../pages/HomePage.tsx";
 import SignUpPage from "../pages/auth/signUp/SignUpPage.tsx";
 import SignInPage from "../pages/auth/signin/SignInPage.tsx";
+import MainLayout from "../layouts/MainLayout.tsx";
 
 const router = createBrowserRouter([
     {
         path: "/",
+        element: <MainLayout />,
         children: [
-            { index: true, element: <HomePage /> },
+            {
+                index: true,
+                element: <HomePage />,
+            },
             {
                 path: "auth",
                 children: [
