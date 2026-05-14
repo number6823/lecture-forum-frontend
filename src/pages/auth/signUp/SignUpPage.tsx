fcfbc734fd7673fa930f419a7783f7c3ed3907b3
number6823 <ohdehet@naver.com>
2026-05-14 15:51:10 +0900

    signUpPage axios로 변경

--- src/pages/auth/signUp/SignUpPage.tsx
+++ src/pages/auth/signUp/SignUpPage.tsx
@@ -5,6 +5,8 @@ import styled from "styled-components";
 import { Gender } from "../../../types/user.type.ts";
 import Button from "../../../components/common/button/Button.tsx";
 import { useNavigate } from "react-router";
+import axiosIntance from "../../../api/axiosIntance.ts";
+import * as axios from "axios";
 
 function SignUpPage() {
     const navigate = useNavigate();
@@ -46,45 +48,31 @@ function SignUpPage() {
         try {
             const { passwordConfirm, ...submitData } = data;
 
-            // submitDate를 백엔드에게 전송 => fetch를 해준다? => 비동기함수네 => async-await => try-catch
-            // fetch(주소,옵션); => 주소는 필수값, 옵션은 선택값
-            // 옵션 객체 {method,header,body}
-            const response = await fetch("http://localhost:8000/user/create", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(submitData), // 객체를 그대로 보낼 수 없고,JSON.stringify()를 통해 JSON형식의 string으로 변환
-            });
-            // response = {ok : boolean, message: string}
-            // response.json ()을 하게 되면 백엔드에서 응답한 내용인 response.message를 JSON으로 파싱
-            // response도 http 메세지 내용이 기록되긱 떄문에 string을 JSON으로 파싱해야 함
-            const result = await response.json();
+            // fetch()로 통신을 하면, 백엔드가 전닳해주는 response가 존재하기만 하면 성공으로 판단하지만
+            // axios로 통신을 하면, 백엔드가 2xx번대 성공 코드를 전달해줘야만 성공으로 판단
+            // 이외의 에러는 catch 처리됨
+            await axiosIntance.post("/user/create", submitData);
 
-            // response.ok 프로퍼티 안에 response 상태 코드가 200번대라면 true, 아니라면 false
-            // throw 키워드는 예외를 발생시켜  catch로 내가 임의적으로 보내는 것
-            if (!response.ok) {
-                throw new Error(result.message || "회원가입 중 오류가 발생했습니다.");
-            }
+            // 성공을 했었을 때 백엔드가 전달해준 내용은 response.data에 객체 상태로 존재함 (JSON 파싱할 필요 없음)
+
             // 백엔드에게 전송해서 성공
             alert("회원가입이 완료되었습니다. 로그인을 진행해주세요.");
             navigate("/auth/signin");
         } catch (error) {
-            if (error instanceof Error) {
-                const errorMessage = error.message;
-                if (errorMessage === "이미 사용 중인 아이디입니다.") {
-                    setError("username", { message: errorMessage });
-                } else if (errorMessage === "이미 가입된 이메일입니다.") {
-                    setError("email", { message: errorMessage });
-                } else if (errorMessage === "이미 사용 중인 닉네임입니다.") {
-                    setError("nickname", { message: errorMessage });
-                } else {
-                    setError("root", { message: errorMessage });
-                }
+            let errorMessage = "회원가입 중 오류가 발생했습니다.";
+
+            // 지금 catch된 error가 axios의 에러인지 판별
+            if (axios.isAxiosError(error)) {
+                // axios에서 발생된 에러라면, 백엔드에서 제공을 한 내용이 error.response.data.message에 존재
+                // 그 백엔드에서 전달해준 내용을 errorMessage에 저장
+                errorMessage = error.response?.data?.message || errorMessage;
+            } else if (error instanceof Error) {
+                // axios에서 발생한 에러가 아닌, 자바스크립트 표준 에러 객체라면
+                // error.message에 담긴 에러 내용을 errorMessage에 저장
+                errorMessage = error.message;
             }
-            // 백엔드에게 전송해서 실패
-            console.log(error);
-            setError("root", { message: "회원가입에 실패했습니다. 다시 시도해주세요." });
+
+            setError("root", { message: errorMessage });
         }
     };
 
@@ -104,7 +92,7 @@ function SignUpPage() {
                             id={"username"}
                             placeholder={"4자 이상 필요"}
                         />
-                        {errors.username && <ErrorMessage>{errors.username.message}</ErrorMessage>}
+                        {errors.username && <E>{errors.username.message}</E>}
                     </InputGroup>
                     <InputGroup>
                         <Label htmlFor={"password"}>비밀번호</Label>
@@ -190,6 +178,8 @@ function SignUpPage() {
                         {errors.gender && <ErrorMessage>{errors.gender.message}</ErrorMessage>}
                     </InputGroup>
                 </FormBox>
+
+                {errors.root && <RootErrorMessage>{errors.root.message}</RootErrorMessage>}
 
                 <Button
                     color={"primary"}
@@ -302,3 +292,11 @@ const ErrorMessage = styled.span`
     color: ${props => props.theme.colors.error};
     font-weight: 500;
 `;
+
+const RootErrorMessage = styled.p`
+ font-size: 14px;
+    text-align: center;
+    color: ${props=> props.theme.colors.error};
+    font-weight: 500;
+    margin-bottom: 50px; 
+`
